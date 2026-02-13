4b346018b0c2e5212d5288d5e475a12ae4c12369
Habiba Elhadi <habibaelhadi9@gmail.com>
2026-02-13 02:17:00 +0200

    Add `AddNewHeader` constant widget

---
+++ lib/core/constants/add_new_header/add_new_header.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:traveller/core/constants/profile_settings/profile_settings_tile_segmented_toggle.dart';
+import 'package:traveller/core/theme/fonts/app_text_styles.dart';
+import '../../theme/colors/app_colors.dart';
+
+class AddNewHeader extends StatelessWidget{
+  final String imageUrl;
+  final String location;
+  final bool isEventPage;
+
+  const AddNewHeader({
+    super.key,
+    required this.imageUrl,
+    required this.location,
+    required this.isEventPage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        CircleAvatar(
+          radius: 20.r,
+          backgroundImage: NetworkImage(imageUrl),
+        ),
+        SizedBox(width: 4.w),
+        Icon(
+          Icons.location_pin,
+          color: AppColors.spanishGrey,
+          size: 8.sp,
+        ),
+        SizedBox(width: 4.w),
+        Text(
+          location,
+          style: AppTextStyles.smallText.copyWith(
+            color: AppColors.spanishGrey
+          )
+        ),
+        if(isEventPage)...[
+         ProfileSettingsTileSegmentedToggle(
+             options: ["Paid","Not Paid"],
+             selectedIndex: 0,
+             onChanged: (value) {}
+         )
+        ]
+      ]
+    );
+  }
+}
