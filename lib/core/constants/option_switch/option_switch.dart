9fb986dc71ae8674677aab045342faa9090a5fba
Habiba Elhadi <habibaelhadi9@gmail.com>
2026-02-13 02:36:24 +0200

    Add reusable `OptionSwitch` widget

---
+++ lib/core/constants/option_switch/option_switch.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:traveller/core/theme/fonts/app_text_styles.dart';
+import '../../theme/colors/app_colors.dart';
+
+class OptionSwitch extends StatefulWidget{
+  final String title;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  const OptionSwitch({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  State<OptionSwitch> createState() => _OptionSwitchState();
+}
+class _OptionSwitchState extends State<OptionSwitch>{
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Switch(
+            value: widget.value,
+            onChanged: widget.onChanged,
+            activeThumbColor: AppColors.turnbullBlue,
+            activeTrackColor: AppColors.deepMarine,
+        ),
+        SizedBox(width: 10.w),
+        Text(
+            widget.title,
+            style: AppTextStyles.titles.copyWith(
+              color: AppColors.black
+            )
+        )
+      ]
+    );
+  }
+}
